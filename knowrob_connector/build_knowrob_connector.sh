5aef07032c5118cbfca50dea312304a5e5215ae0
Giang Nguyen <hoanggia@uni-bremen.de>
2025-04-08 15:24:06 +0200

    Update CMake

--- knowrob_connector/build_knowrob_connector.sh
+++ knowrob_connector/build_knowrob_connector.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-cmake -S $PWD -B build -DCMAKE_INSTALL_PREFIX:PATH=$PWD
+MULTIVERSE_DIR="$PWD/../../.."
+CMAKE_EXECUTABLE=$MULTIVERSE_DIR/build/CMake/bin/cmake
+if [ ! -f "$CMAKE_EXECUTABLE" ]; then
+    CMAKE_EXECUTABLE=$(which cmake)
+fi
+if [ ! -f "$CMAKE_EXECUTABLE" ]; then
+    echo "cmake does not exist."
+    exit 1
+fi
+
+echo "Building KnowRob Connector using CMake: $CMAKE_EXECUTABLE"
+$CMAKE_EXECUTABLE -S $PWD -B build -DCMAKE_INSTALL_PREFIX:PATH=$PWD
 make -C build
-cmake --install build
+$CMAKE_EXECUTABLE --install build
